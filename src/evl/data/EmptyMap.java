23c550355fff7185e0ac03f8deaa3a492bc5d356
legoc <none@none>
2014-05-06 23:32:46 +0200

    added tutorial 7

---
+++ src/evl/data/EmptyMap.java
@@ -0,0 +1,84 @@
+package evl.data;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+
+public class EmptyMap<V, K> implements Map<K, V> {
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V get(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<K> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(K arg0, V arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
---
+++ src/tutorials/tutorial7/Main.java
@@ -0,0 +1,79 @@
+package tutorials.tutorial7;
+
+import java.util.HashMap;
+
+import tutorials.classes.A;
+import tutorials.classes.B;
+import tutorials.classes.C;
+import evl.base.Method1;
+import evl.base.Method2;
+import evl.data.DispatchableMethodD;
+import evl.data.EmptyMap;
+import evl.data.Method2D;
+import evl.data.PrioritySymmetricComparator;
+
+/**
+ * Example with different cache strategies.
+ * @author yan
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		A b = new B(1, 2);
+		A c = new C(2, -5);
+		
+		Foo foo = new Foo();
+		
+		Method1<Integer> process = Method1.<Integer>builder()
+						.unboundedCache()
+						.build()
+						.addAll(Foo.class, "process", foo);
+		
+		System.out.println(process.invoke(b));
+		System.out.println(process.invoke(c));
+		
+		
+		process = Method1.<Integer>builder()
+				.boundedCache(2)
+				.build()
+				.addAll(Foo.class, "process", foo);
+
+		System.out.println(process.invoke(b));
+		System.out.println(process.invoke(c));
+		
+		
+		process = Method1.<Integer>builder()
+				.cache(new HashMap<Class<?>, DispatchableMethodD<Void>>())
+				.build()
+				.addAll(Foo.class, "process", foo);
+
+		System.out.println(process.invoke(b));
+		System.out.println(process.invoke(c));
+		
+		
+		Method2<Integer> process2 = Method2.<Integer>builder()
+				.cache(new HashMap<Method2.ClassTuple, DispatchableMethodD<Void>>())
+				.build()
+				.addAll(Foo.class, "process2", foo);
+
+
+		System.out.println(process2.invoke(b, c));
+
+		
+		Method2D<Integer, Integer> process2d = Method2D.<Integer, Integer>builder()
+				.comparator(new PrioritySymmetricComparator<Integer>())
+				.cache(new HashMap<Method2D.ClassTuple, DispatchableMethodD<Integer>>())
+				.build()
+				.add(Foo.class.getMethod("process2", B.class, A.class), foo, 1)
+				.add(Foo.class.getMethod("process2", B.class, C.class), foo, 2);
+
+		System.out.println(process2d.invoke(b, c));
+
+		
+		EmptyMap<Integer, Integer> em = new EmptyMap<Integer, Integer>();
+		em.put(1, 2);
+		System.out.println("em 1 = " + em.get(1));
+	}
+}
