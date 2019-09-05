8eeabec721b544c724d0c3de266ce9b2d07b1164
ylegoc <ylegoc@gmail.com>
2019-09-05 21:08:10 +0200

    Renamed tutorials into examples

---
+++ evl-examples/src/main/java/eu/daproject/evl/Examples.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright 2019 The EVL authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package eu.daproject.evl;
+
+import eu.daproject.evl.asteroid.Collider;
+import eu.daproject.evl.example1.Example1;
+import eu.daproject.evl.example10.Example10;
+import eu.daproject.evl.example11.Example11;
+import eu.daproject.evl.example2.Example2;
+import eu.daproject.evl.example3.Example3;
+import eu.daproject.evl.example4.Example4;
+import eu.daproject.evl.example5.Example5;
+import eu.daproject.evl.example6.Example6;
+import eu.daproject.evl.example7.Example7;
+import eu.daproject.evl.example8.Example8;
+import eu.daproject.evl.example9.Example9;
+
+public class Examples {
+
+	public static void main(String[] args) throws Throwable {
+	
+		System.out.println("EVL Tutorials");
+		
+		System.out.println("");
+		System.out.println("*** Asteroid ***");
+		Collider.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 1 ***");
+		Example1.run();
+		System.out.println("");
+
+		System.out.println("");
+		System.out.println("*** Example 2 ***");
+		Example2.run();
+		System.out.println("");
+
+		System.out.println("");
+		System.out.println("*** Example 3 ***");
+		Example3.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 4 ***");
+		Example4.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 5 ***");
+		Example5.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 6 ***");
+		Example6.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 7 ***");
+		Example7.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 8 ***");
+		Example8.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 9 ***");
+		Example9.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 10 ***");
+		Example10.run();
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("*** Example 11 ***");
+		Example11.run();
+		System.out.println("");
+	}
+}
---
+++ evl-examples/src/main/java/eu/daproject/evl/example6/Example6.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2019 The EVL authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package eu.daproject.evl.example6;
+
+import eu.daproject.evl.classes.A;
+import eu.daproject.evl.classes.B;
+import eu.daproject.evl.classes.C;
+
+/**
+ * "Open-method" example. A singleton multimethod can be configured anywhere.
+ *
+ */
+public class Example6 {
+	
+	public static class Foo1 {
+
+		public int match(B obj) {
+			return 1 + obj.getB();
+		}
+	}
+	
+	public static class Foo2 {
+
+		public int match(C obj) {
+			return 2 + obj.getC();
+		}
+	}
+	
+	public static void run() throws Throwable {
+		
+		A b = new B(1, 2);
+		A c = new C(2, -5);
+		
+		Foo1 foo1 = new Foo1();
+		Process.instance().add(foo1);
+		
+		Foo2 foo2 = new Foo2();
+		Process.instance().add(foo2);
+		
+		System.out.println(Process.instance().invoke(b));
+		System.out.println(Process.instance().invoke(c));
+	}
+}
