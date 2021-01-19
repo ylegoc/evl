2985c85bec25f074f4d000568380f4e5cea8c2a4
ylegoc <ylegoc@gmail.com>
2021-01-19 21:46:27 +0100

    Print example

--- evl-examples/src/main/java/eu/daproject/evl/examples/printer/Print.java
+++ evl-examples/src/main/java/eu/daproject/evl/examples/printer/Print.java
@@ -1,5 +1,6 @@
 package eu.daproject.evl.examples.printer;
 
+import eu.daproject.evl.Cases;
 import eu.daproject.evl.Method2;
 
 public class Print {
@@ -10,6 +11,14 @@ public class Print {
 	
 	public static Method2<String> method() {
 		return method;
+	}
+	
+	static {
+		method.add(new Cases() {
+			String match(Object format, Object object) {
+				return object.toString();
+			}
+		});
 	}
 	
 }
